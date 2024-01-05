5307891754c64cfc36a38ed328ae23fd9e91155d
井上祐斗 <inyt113@inyt-Mac.local>
2024-01-05 10:33:19 +0900

    layout.tsxでmetadataの設定をした

---
+++ app/layout.tsx
@@ -0,0 +1,51 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { FC, ReactNode } from "react";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
+
+type RootLayoutProps = {
+  children: ReactNode;
+};
+const RootLayout: FC<RootLayoutProps> = ({ children }) => {
+  return (
+    <html lang="jp">
+      <head />
+      <body>
+        <Header />
+        <main className="min-h-full grow">{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
+
+const domain = new URL(
+  process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : `http://localhost:${process.env.PORT || 3000}`
+);
+
+export const metadata: Metadata = {
+  metadataBase: domain,
+
+  title: {
+    default: "Mobazy",
+    template: "%s | Mobazy",
+  },
+  description: "バッテリーの貸し出しサービス",
+  openGraph: {
+    // Open graph image will be provided via file-based configuration.
+    // Refer: https://beta.nextjs.org/docs/api-reference/metadata#static-images
+    type: "website",
+    locale: "ja_JP",
+    url: domain,
+  },
+  // themeColor: [
+  //   { media: '(prefers-color-scheme: light)', color: colorTokens.keyplate.light['2'] }, // keyplate-light-2
+  //   { media: '(prefers-color-scheme: dark)', color: colorTokens.keyplate.dark['2'] }, // keyplate-dark-2
+  // ],
+};
