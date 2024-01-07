53fcbbe6b643be76219e4cb5be4b32f62e524e3b
井上祐斗 <inyt113@inyt-Mac.local>
2024-01-07 18:06:43 +0900

    shadcn-uiの導入、avaterの追加

--- components/NavLinks/NavLinks.tsx
+++ components/NavLinks/NavLinks.tsx
@@ -3,46 +3,57 @@
 import type { FC } from "react";
 import { useMemo } from "react";
 import { Link } from "@/components/Link/Link";
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export const NavLinks: FC = () => {
   //   const user = useUser();
   //   const hasUserLoggedIn = useMemo(() => user?.isSignedIn, [user]);
-  const hasUserLoggedIn = false;
+  const hasUserLoggedIn = true;
   return (
     <ul className="flex shrink grow flex-row items-center justify-between truncate">
       <li className="inline-flex shrink truncate">
         <Link
-          title="本を探す"
-          href="/book"
-          className="truncate rounded-full px-1.5 py-2 no-underline hover:bg-primary-3 hover:text-primary-11 tablet:px-4"
+          title="マップ"
+          href="/map"
+          className="truncate rounded-full px-1.5 py-2 no-underline hover:bg-opacity-80 hover:text-primary-11 tablet:px-4"
         >
-          本を探す
+          Map
         </Link>
       </li>
       {hasUserLoggedIn ? (
         <>
-          <li className="inline-flex shrink truncate">
-            <Link
-              title="タグを見る"
-              href="/profile"
-              className="truncate rounded-full px-1.5 py-2 no-underline hover:bg-primary-3 hover:text-primary-11 tablet:px-4"
-            >
-              プロフィール
-            </Link>
-          </li>
-          <li>{/* <UserButton afterSignOutUrl="/" /> */}</li>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Avatar>
+                <AvatarImage
+                  src="https://github.com/shadcn.png"
+                  className="mr-2"
+                />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>inoueyt113@gmail.com</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>ご利用履歴</DropdownMenuItem>
+              <DropdownMenuItem>ログアウト</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </>
       ) : (
         <>
-          <li className="inline-flex shrink truncate">
-            <Link
-              title="ログイン"
-              href="/sign-in/"
-              className="truncate rounded-full px-1.5 py-2 no-underline hover:bg-primary-3 hover:text-primary-11 tablet:px-4"
-            >
-              ログイン
-            </Link>
-          </li>
+          <Button asChild>
+            <Link href="/login">Login</Link>
+          </Button>
         </>
       )}
     </ul>
